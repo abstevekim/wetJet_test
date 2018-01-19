61f207d4eeb97f7efbd9beb8c21174276333c02c
abstevekim <abstevekim@mail.com>
2018-01-19 08:42:28 +0000

    math

---
+++ add_get.java
@@ -0,0 +1,19 @@
+	@ResponseBody
+	@RequestMapping(value="/add", method = {RequestMethod.GET})
+	public SessionInfo add(@RequestParam Integer num_1, @RequestParam Integer num_2, HttpServletRequest req) {
+
+		SessionInfo sessionInfo = new SessionInfo();
+		
+		try {
+				java.math.BigDecimal result_value = new BigDecimal(num_1.floatValue()).add(new BigDecimal(num_2.floatValue()));
+		
+				sessionInfo.setSessionInfoWithOK(null, "result", result_value);
+		
+				
+		} catch(Exception ex) {
+			logger.fatal(ex.getMessage());
+			sessionInfo.setSessionInfoWithMethodFailure(null);
+		}
+
+		return sessionInfo;
+	}
